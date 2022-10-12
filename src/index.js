edbaea5794e6dd9e52d5b2a4905e344074ca2b5d
v4lomyr <bintangdwitama160501@gmail.com>
2022-10-12 10:12:22 +0700

    🐱‍💻Passing data through props

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
