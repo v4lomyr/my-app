595d4a5e17d3dcd15b39c6d0d35f43fc457ba428
v4lomyr <bintangdwitama160501@gmail.com>
2022-10-12 10:25:25 +0700

    🐱‍💻Interactivity

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
